24e6e42094904f22a0c427e4d1a6de967d099a27
Scott Bromander <scott@primeacademy.io>
2016-07-19 15:48:30 -0500

    input detection

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -6,11 +6,32 @@ public enum Buttons{
 	Left,
 }
 
+public enum Condition{
+	GreaterThan,
+	LessThan
+}
+
 [System.Serializable]
 public class InputAxisState {
 	public string axisName; //example 'Horizontal'
 	public float offValue; //Is value on or off?
 	public Buttons button; //Our enum from above
+	public Condition condition;
+
+	public bool value{
+		get{
+			var value = Input.GetAxis (axisName);
+
+			switch (condition) {
+			case Condition.GreaterThan:
+				return value > offValue;
+			case Condition.LessThan:
+				return value < offValue;
+			}
+
+			return false;
+		}
+	}
 }
 
 public class InputManager : MonoBehaviour {
@@ -24,6 +45,10 @@ public class InputManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		foreach (var input in inputs) {
+			if (input.value) {
+				Debug.Log ("Input detected: " + input.button);
+			}
+		}
 	}
 }
