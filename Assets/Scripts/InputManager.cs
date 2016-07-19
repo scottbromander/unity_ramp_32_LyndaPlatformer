04426ce4b5a3ecc8d30195899afbbef2df4b0dea
Scott Bromander <scott@primeacademy.io>
2016-07-19 15:28:00 -0500

    initial commit

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Buttons{
+	Right,
+	Left,
+}
+
+[System.Serializable]
+public class InputAxisState {
+	public string axisName; //example 'Horizontal'
+	public float offValue; //Is value on or off?
+	public Buttons button; //Our enum from above
+}
+
+public class InputManager : MonoBehaviour {
+
+	public InputAxisState[] inputs;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/SimpleMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMovement : MonoBehaviour {
+	public float speed = 5f;
+	private Rigidbody2D body2d;
+
+	// Use this for initialization
+	void Start () {
+		body2d = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
