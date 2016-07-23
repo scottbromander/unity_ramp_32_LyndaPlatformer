d426ed104079f96454aac906f6beaab0b07c6140
Scott Bromander <scott@primeacademy.io>
2016-07-22 21:59:02 -0500

    null check on input

--- Assets/Scripts/SimpleMovement.cs
+++ Assets/Scripts/SimpleMovement.cs
@@ -16,16 +16,18 @@ public class SimpleMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		var right = inputState.GetButtonValue (input [0]);
-		var left = inputState.GetButtonValue (input [1]);
-		var velX = speed;
+		if (inputState != null) {
+			var right = inputState.GetButtonValue (input [0]);
+			var left = inputState.GetButtonValue (input [1]);
+			var velX = speed;
 
-		if (right || left) {
-			velX = left ? -1 : 1;
-		} else {
-			velX = 0;
+			if (right || left) {
+				velX = left ? -1 : 1;
+			} else {
+				velX = 0;
+			}
+
+			body2d.velocity = new Vector2 (velX, body2d.velocity.y);
 		}
-
-		body2d.velocity = new Vector2 (velX, body2d.velocity.y);
 	}
 }
