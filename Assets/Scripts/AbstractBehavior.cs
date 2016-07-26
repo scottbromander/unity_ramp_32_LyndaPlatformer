39138a6987333b1f3a553d2e6827a3b8fd52c54d
Scott Bromander <scott@primeacademy.io>
2016-07-26 15:18:26 -0500

    abstract clsas setup

---
+++ Assets/Scripts/AbstractBehavior.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class AbstractBehavior : MonoBehaviour {
+
+	public Buttons[] inputButtons;
+
+	protected InputState inputState;
+	protected Rigidbody2D body2d;
+
+	protected virtual void Awake(){
+		inputState = GetComponent<InputState> ();
+		body2d = GetComponent<Rigidbody2D> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
