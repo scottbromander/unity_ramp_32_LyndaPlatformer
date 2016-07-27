6ff5aafb3ae08fdbb8caba1cc64c136dc31e4ac2
Scott Bromander <scott@primeacademy.io>
2016-07-26 20:59:47 -0500

    run integrated

--- Assets/Scripts/Behaviors/Walk.cs
+++ Assets/Scripts/Behaviors/Walk.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class Walk : AbstractBehavior {
 
 	public float speed = 50f;
+	public float runMultiplier = 2f;
+
 
 	// Use this for initialization
 	void Start () {
@@ -14,9 +16,16 @@ public class Walk : AbstractBehavior {
 	void Update () {
 		var right = inputState.GetButtonValue (inputButtons [0]);
 		var left = inputState.GetButtonValue (inputButtons [1]);
+		var run = inputState.GetButtonValue (inputButtons [2]);
+
+
 
 		if (right || left) {
 			var tempSpeed = speed;
+
+			if (run && runMultiplier > 0) {
+				tempSpeed *= runMultiplier;
+			}
 
 			var velX = tempSpeed * (float)inputState.direction;
 
