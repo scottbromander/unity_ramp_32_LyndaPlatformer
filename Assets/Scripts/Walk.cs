56d59903223f6323a8b1f9483261123b00ef04e3
Scott Bromander <scott@primeacademy.io>
2016-07-26 19:43:04 -0500

    movement

---
+++ Assets/Scripts/Walk.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Walk : AbstractBehavior {
+
+	public float speed = 50f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var right = inputState.GetButtonValue (inputButtons [0]);
+		var left = inputState.GetButtonValue (inputButtons [1]);
+
+		if (right || left) {
+			var tempSpeed = speed;
+
+			var velX = tempSpeed * (float)inputState.direction;
+
+			body2d.velocity = new Vector2 (velX, body2d.velocity.y);
+		}
+	}
+}
