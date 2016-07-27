61f6277484199eec6567b8ea9bd7278b97820fd7
Scott Bromander <scott@primeacademy.io>
2016-07-26 19:04:41 -0500

    facing setup

---
+++ Assets/Scripts/FaceDirection.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceDirection : AbstractBehavior {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var right = inputState.GetButtonValue (inputButtons [0]);
+		var left = inputState.GetButtonValue (inputButtons [1]);
+
+		if (right) {
+			print ("right!");
+		} else {
+			print ("left!");
+		}
+	}
+}
