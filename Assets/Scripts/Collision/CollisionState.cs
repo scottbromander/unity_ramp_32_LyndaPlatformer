1430d59a7701f7512d55b95c4b70221439a45904
Scott Bromander <scott@primeacademy.io>
2016-07-28 20:32:29 -0500

    add collision state

---
+++ Assets/Scripts/Collision/CollisionState.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionState : MonoBehaviour {
+
+	public LayerMask collisionLayer;
+	public bool standing;
+	public Vector2 bottomPosition = Vector2.zero;
+	public float collisionRadius = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate(){
+		var pos = bottomPosition;
+		pos.x += transform.position.x;
+		pos.y += transform.position.y;
+
+		standing = Physics2D.OverlapCircle (pos, collisionRadius, collisionLayer);
+	}
+}
