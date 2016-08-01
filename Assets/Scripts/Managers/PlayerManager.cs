330b9d139f841ff4110b1abc7742a426f05b0bc4
Scott Bromander <scott@primeacademy.io>
2016-07-31 20:37:30 -0500

    collision state wrap up

--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -6,11 +6,13 @@ public class PlayerManager : MonoBehaviour {
 	private InputState inputState;
 	private Walk walkBehavior;
 	private Animator animator;
+	private CollisionState collisionState;
 
 	void Awake() {
 		inputState = GetComponent<InputState> ();
 		walkBehavior = GetComponent<Walk> ();
 		animator = GetComponent<Animator> ();
+		collisionState = GetComponent<CollisionState> ();
 	}
 
 	// Use this for initialization
@@ -20,7 +22,7 @@ public class PlayerManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (inputState.absVelX == 0) {
+		if (collisionState.standing) {
 			ChangeAnimationState (0);
 		}
 
