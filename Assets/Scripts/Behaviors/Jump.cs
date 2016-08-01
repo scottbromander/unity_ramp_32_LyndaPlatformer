a6e2570fdc5dbaf9019893ee617bcb4ee6d29268
Scott Bromander <scott@primeacademy.io>
2016-07-31 20:48:09 -0500

    character jump basic integration

---
+++ Assets/Scripts/Behaviors/Jump.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : AbstractBehavior {
+
+	public float jumpSpeed = 200f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var canJump = inputState.GetButtonValue (inputButtons [0]);
+
+		if (collisionState.standing) {
+			if (canJump) {
+				OnJump ();
+			}
+		}
+	}
+
+	protected virtual void OnJump(){
+		var vel = body2d.velocity;
+
+		body2d.velocity = new Vector2 (vel.x, jumpSpeed);
+	}
+}
