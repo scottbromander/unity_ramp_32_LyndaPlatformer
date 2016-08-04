62b5ef16642c5bbabd269fe42cb591089c3164d7
Scott Bromander <scott@primeacademy.io>
2016-08-03 20:25:33 -0500

    add duck

--- Assets/Scripts/Behaviors/Duck.cs
+++ Assets/Scripts/Behaviors/Duck.cs
@@ -17,17 +17,34 @@ public class Duck : AbstractBehavior {
 		originalCenter = circleCollider.offset;
 	}
 
-	protected vitual void OnDuck(bool value){
+	protected virtual void OnDuck(bool value){
+		ducking = value;
 
-	}
+		var size = circleCollider.radius;
 
-	// Use this for initialization
-	void Start () {
-	
+		float newOffsetY;
+		float sizeReciprocal;
+
+		if (ducking) {
+			sizeReciprocal = scale;
+			newOffsetY = circleCollider.offset.y - size / 2 + centerOffsetY;
+		} else {
+			sizeReciprocal = 1 / scale;
+			newOffsetY = originalCenter.y;
+		}
+
+		size = size * sizeReciprocal;
+		circleCollider.radius = size;
+		circleCollider.offset = new Vector2 (circleCollider.offset.x, newOffsetY);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		var canDuck = inputState.GetButtonValue (inputButtons [0]);
+		if (canDuck && collisionState.standing && !ducking) {
+			OnDuck (true);
+		} else if(ducking && !canDuck) {
+			OnDuck (false);
+		}
 	}
 }
