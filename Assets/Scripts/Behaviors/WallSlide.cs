d2ac08bc6ea1cb0d0983652af07b9aaa060e6d3e
Scott Bromander <scott@primeacademy.io>
2016-08-06 21:28:12 -0500

    wall slide

---
+++ Assets/Scripts/Behaviors/WallSlide.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallSlide : StickToWall {
+
+	public float slideVelocity = -5f;
+
+
+	// Update is called once per frame
+	override protected void Update () {
+		base.Update ();
+
+		if (onWallDetected) {
+			var velY = slideVelocity;
+
+			body2d.velocity = new Vector2 (body2d.velocity.x, velY);
+		}
+	}
+
+	override protected void OnStick(){
+		body2d.velocity = Vector2.zero;
+	}
+
+	override protected void OffWall(){
+
+		//Makes it do nothing
+	}
+}
