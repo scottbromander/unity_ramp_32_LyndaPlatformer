65b2c3a27b1f1c298df5c54563d5d51ac4fe663e
Scott Bromander <scott@primeacademy.io>
2016-08-06 19:56:10 -0500

    camera smooth follow

--- Assets/Scripts/Behaviors/CameraBehavior.cs
+++ Assets/Scripts/Behaviors/CameraBehavior.cs
@@ -5,21 +5,14 @@ public class CameraBehavior : MonoBehaviour {
 	public Camera camera;
 	public GameObject player;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+	private Vector3 velocity = Vector3.zero;
+	public float followValue = 0.15f;
 
 	// Update is called once per frame
-	void FixedUpdate () {
+	void LateUpdate () {
 		var currentCameraPosition = new Vector3 (camera.transform.position.x, camera.transform.position.y, camera.transform.position.z);
-		var currentPlayerPosition = new Vector3 (player.transform.position.x, player.transform.position.y, player.transform.position.z);
+		var currentPlayerPosition = new Vector3 (player.transform.position.x, player.transform.position.y, camera.transform.position.z);
 
-		var newX = currentCameraPosition.x + (-(currentCameraPosition.x - currentPlayerPosition.x) / 2);
-		var newY = currentCameraPosition.y + (-(currentCameraPosition.y - currentPlayerPosition.y) / 2);
-
-		var newCameraPosition = new Vector3 (newX, newY, currentCameraPosition.z);
-
-		camera.transform.position = newCameraPosition;
+		camera.transform.position = Vector3.SmoothDamp(currentCameraPosition, currentPlayerPosition, ref velocity, followValue);
 	}
 }
