155ce472508d0ccd1872d3fcdf1b6fe864d3ded0
Scott Bromander <scott@primeacademy.io>
2016-08-06 20:49:06 -0500

    player collidors for wall

--- Assets/Scripts/Collision/CollisionState.cs
+++ Assets/Scripts/Collision/CollisionState.cs
@@ -5,13 +5,18 @@ public class CollisionState : MonoBehaviour {
 
 	public LayerMask collisionLayer;
 	public bool standing;
+	public bool onWall;
 	public Vector2 bottomPosition = Vector2.zero;
+	public Vector2 rightPosition = Vector2.zero;
+	public Vector2 leftPosition = Vector2.zero;
 	public float collisionRadius = 10f;
 	public Color debugCollisionColor = Color.red;
 
+	private InputState inputState;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+		inputState = GetComponent<InputState> ();
 	}
 	
 	// Update is called once per frame
@@ -20,20 +25,33 @@ public class CollisionState : MonoBehaviour {
 	}
 
 	void FixedUpdate(){
+
+
+
 		var pos = bottomPosition;
 		pos.x += transform.position.x;
 		pos.y += transform.position.y;
 
 		standing = Physics2D.OverlapCircle (pos, collisionRadius, collisionLayer);
+
+		pos = inputState.direction == Directions.Right ? rightPosition : leftPosition;
+		pos.x += transform.position.x;
+		pos.y += transform.position.y;
+
+		onWall = Physics2D.OverlapCircle (pos, collisionRadius, collisionLayer);
 	}
 
 	void OnDrawGizmos() {
 		Gizmos.color = debugCollisionColor;
 
-		var pos = bottomPosition;
-		pos.x += transform.position.x;
-		pos.y += transform.position.y;
+		var positions = new Vector2[] { rightPosition, bottomPosition, leftPosition };
 
-		Gizmos.DrawWireSphere (pos, collisionRadius);
+		foreach (var position in positions) {
+			var pos = position;
+			pos.x += transform.position.x;
+			pos.y += transform.position.y;
+
+			Gizmos.DrawWireSphere (pos, collisionRadius);
+		}
 	}
 }
