4ac8ffadbba5ce29b9d6d933cb6e015cf41cf914
Scott Bromander <scott@primeacademy.io>
2016-08-08 17:31:15 -0500

    course complete

--- Assets/Scripts/Behaviors/FireProjectile.cs
+++ Assets/Scripts/Behaviors/FireProjectile.cs
@@ -5,6 +5,9 @@ public class FireProjectile : AbstractBehavior {
 
 	public float shootDelay = 0.5f;
 	public GameObject projectilePrefab;
+	public Vector2 firePosition = Vector2.zero;
+	public Color debugColor = Color.yellow;
+	public float debugRadius = 3f;
 
 	private float timeElapsed = 0;
 
@@ -13,7 +16,7 @@ public class FireProjectile : AbstractBehavior {
 			var canFire = inputState.GetButtonValue (inputButtons [0]);
 
 			if (canFire && timeElapsed > shootDelay) {
-				CreateProjectile (transform.position);
+				CreateProjectile (CalculateFirePosition());
 				timeElapsed = 0;
 			}
 
@@ -21,9 +24,29 @@ public class FireProjectile : AbstractBehavior {
 		}
 	}
 
+	Vector2 CalculateFirePosition(){
+		var pos = firePosition;
+		pos.x *= (float)inputState.direction;
+		pos.x += transform.position.x;
+		pos.y += transform.position.y;
+		return pos;
+	}
+
 	public void CreateProjectile(Vector2 pos){
 		var clone = Instantiate (projectilePrefab, pos, Quaternion.identity) as GameObject;
 		clone.transform.localScale = transform.localScale;
 
+	}
+
+	void OnDrawGizmos(){
+		Gizmos.color = debugColor;
+		var pos = firePosition;
+		if (inputState != null) {
+			pos.x *= (float)inputState.direction;
+		}
+		pos.x += transform.position.x;
+		pos.y += transform.position.y;
+
+		Gizmos.DrawWireSphere(pos, debugRadius);
 	}
 }
