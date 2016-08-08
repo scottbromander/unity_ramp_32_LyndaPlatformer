efcb541b1b9565d5a38a2cc2bd6c339226cf3752
Scott Bromander <scott@primeacademy.io>
2016-08-07 22:48:30 -0500

    fireball setup

---
+++ Assets/Scripts/Items/FireBall.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireBall : MonoBehaviour {
+
+	public Vector2 initialVelocity = new Vector2 (100, -100);
+	private Rigidbody2D body2d;
+
+	void Awake(){
+		body2d = GetComponent<Rigidbody2D> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		var startVelX = initialVelocity.x * transform.localScale.x;
+		body2d.velocity = new Vector2 (startVelX, initialVelocity.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
