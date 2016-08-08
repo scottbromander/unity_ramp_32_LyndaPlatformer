3efff0a235ee8f9dc9431a42f4b0cb870374ae84
Scott Bromander <scott@primeacademy.io>
2016-08-07 22:15:19 -0500

    collect flower

--- Assets/Scripts/Items/PowerUpFlower.cs
+++ Assets/Scripts/Items/PowerUpFlower.cs
@@ -3,13 +3,12 @@ using System.Collections;
 
 public class PowerUpFlower : Collectable {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public int itemID = 1;
+
+	override protected void OnCollect(GameObject target){
+		var equipedBehavior = target.GetComponent<Equip> ();
+		if (equipedBehavior != null) {
+			equipedBehavior.currentItem = itemID;
+		}
 	}
 }
