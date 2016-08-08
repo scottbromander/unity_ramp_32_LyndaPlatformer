326df5ab0f254d5c96b89a47223752f4494d97f3
Scott Bromander <scott@primeacademy.io>
2016-08-08 12:36:54 -0500

    shoot fireball after collect

---
+++ Assets/Scripts/Behaviors/FireProjectile.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireProjectile : AbstractBehavior {
+
+	public float shootDelay = 0.5f;
+	public GameObject projectilePrefab;
+
+	private float timeElapsed = 0;
+
+	void Update(){
+		if (projectilePrefab != null) {
+			var canFire = inputState.GetButtonValue (inputButtons [0]);
+
+			if (canFire && timeElapsed > shootDelay) {
+				CreateProjectile (transform.position);
+				timeElapsed = 0;
+			}
+
+			timeElapsed += Time.deltaTime;
+		}
+	}
+
+	public void CreateProjectile(Vector2 pos){
+		var clone = Instantiate (projectilePrefab, pos, Quaternion.identity) as GameObject;
+		clone.transform.localScale = transform.localScale;
+
+	}
+}
--- Assets/Scripts/Items/PowerUpFlower.cs
+++ Assets/Scripts/Items/PowerUpFlower.cs
@@ -4,11 +4,17 @@ using System.Collections;
 public class PowerUpFlower : Collectable {
 
 	public int itemID = 1;
+	public GameObject projectilePrefab;
 
 	override protected void OnCollect(GameObject target){
 		var equipedBehavior = target.GetComponent<Equip> ();
 		if (equipedBehavior != null) {
 			equipedBehavior.currentItem = itemID;
+		}
+
+		var shootBehavior = target.GetComponent<FireProjectile> ();
+		if (shootBehavior != null) {
+			shootBehavior.projectilePrefab = projectilePrefab;
 		}
 	}
 }
